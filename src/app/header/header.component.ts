053d1fa694c8ec554c9671e37b9bcd7d0a8c0fd5
Gil Barkan <barkan.gil@gmail.com>
2019-08-21 16:16:22 +0200

    angular-practice-1/routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,15 +7,10 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  @Output() featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
   }
 
 }
